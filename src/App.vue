45f52c9f11b1597e92aeed19e40ffc1436113fe6
willian <hellowillian21@gmail.com>
2019-04-07 23:03:02 +0800

    分页组件1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="list">
+    <template v-if="count">
+      <ul>
+        <li v-for="item in items" :key="item.id">...</li>
+      </ul>
+      <Paging
+        :perPages='perPages'
+        :pageIndex="currentPage"
+        :total="count"
+        :pageSize="pageSize"
+        @change="pageChange"
+      >
+      </Paging>
+    </template>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Paging from "./components/Paging";
 export default {
+  //显示的声明组件
   name: 'app',
   components: {
-    HelloWorld
+    Paging
+  },
+  data() {
+    return {
+      perPages: 5,
+      pageSize: 20, //每页显示20条数据
+      currentPage: 1, //当前页码
+      count: 200, //总记录数
+      items: []
+    };
+  },
+  methods: {
+    //获取数据
+    getList() {
+
+    },
+
+    //从paging组件传递过来的当前page
+    pageChange(page) {
+      this.currentPage = page;
+      this.getList();
+    }
+  },
+  mounted() {
+    //请求第一页数据
+    this.getList();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.list {
+  display: flex;
+  justify-content: center;
 }
 </style>
